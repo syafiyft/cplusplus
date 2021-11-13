cd0b477220ff6418b8b009f39bd55305db1822da
syafiyft <syafiyft@gmail.com>
2021-11-13 15:03:29 +0800

    SS1 Basics Prog

---
+++ SS C++/SS1 Basics-Programming.cpp
@@ -0,0 +1,16 @@
+//library - tools to achieve our goals
+//we can create library but that is for advanced
+#include <iostream> //IOstream stands for input-output stream
+
+using namespace std; //dont need to write std:: for eact cout cin
+main() //function - something that is created to function, parenthesis is for parameter
+{
+    cout << "this is test";
+    //COut stands for Console Output
+    //<< sign are pointing towards the COut, it means that everything after withing the space
+    //be sent to COUT.
+    //COUT is part of namespace because its a space that has name
+    //COUT is from std namespace
+    //can be like this std::cout
+
+}
