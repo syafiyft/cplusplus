374683fb231de64cef37e992f50638029e1b0716
syafiyft <syafiyft@gmail.com>
2021-11-25 23:22:46 +0800

    SS15 For Loops

---
+++ SS C++/SS15 For Loops.cpp
@@ -0,0 +1,33 @@
+#include <iostream>
+//LOOPS
+using namespace std;
+
+main()
+{
+    /*
+
+    for (initialization; condition; inc/dec)
+        instruction-to-repeat
+    */
+
+    /*for (int i = 0; i < 3; i++)
+        cout << i << endl;
+
+    cout << "lala" << endl;
+    */
+/*
+    int array[100];
+
+    for (int i = 0; i < 100; i++)
+    {
+        array[i] = i;
+        cout << array[i] << endl;
+    }
+
+*/
+
+        //INFINITE LOOP
+
+    for (;;) //Bad PRACTICE
+        cout << "lala" << endl;
+}
