dbf8aed40c696e52f5b595314c64fec39e24fff3
syafiyft <syafiyft@gmail.com>
2021-11-26 18:00:00 +0800

    update Functions

--- SS C++/SS20 Functions.cpp
+++ SS C++/SS20 Functions.cpp
@@ -5,61 +5,62 @@ using namespace std;
 
 //DECLARATION OF FUNCTION
 void welcome(); //in parenthesis we called it parameter @ procedure
-bool isNumber();
-
-
-main()
+bool isNumber(string tmp); //can just write string
+void enterName();
+double add(double a, double b) {return a + b;}
+void changeValueTo10(int x)
 {
-    welcome(); //CALL function
-    string tmp; //if ask for integer, why use string? string are characters in ASCII
+    x = 10;
+}
 
-    cout << "Enter the number: " << endl;
-    cin >> tmp;
+int main()
+{
+    /*welcome(); //CALL function
+    enterName();
+    cout << add(4,5);
+    */
+    int a = 5;
 
-    bool isNumber = true; //indicate true or false.
+    changeValueTo10(a); //we assigned a to x, x = a @ x = 5
 
-    if (tmp[0] == '0')
-        isNumber = false; //hey this is not Number
-    for (int i = 0; i < (tmp.length() && isNumber); i++) //tmp.length() - tmp is variable, length() is function and the . is connector
-    {
-        cout << tmp[i] << endl; //check all character entered
-        if(!(tmp[i] >= 48 && tmp[i] <= 57)) // we used NEGATION ! //checking if set of number (IN DECIMAL) is between 0-9. i.e 48 is 0
-        {
-            isNumber = false;
-            break;
-        }
-    }
+    cout << a;
 
-    if (isNumber) //if TRUE
-        cout << "Number entered properly " << endl;
-    else //if FALSE
-        cout << "Number wasn't entered properly" << endl;
-
-    cout << "Enter the number: " << endl;
-    cin >> tmp;
-
-    isNumber = true; //indicate true or false.
-
-    if (tmp[0] == '0')
-        isNumber = false;
-    for (int i = 0; i < (tmp.length()&& isNumber); i++) //tmp.length() - tmp is variable, length() is function and the . is connector
-    {
-        cout << tmp[i] << endl; //check all character entered
-        if(!(tmp[i] >= 48 && tmp[i] <= 57)) // we used NEGATION ! //checking if set of number (IN DECIMAL) is between 0-9. i.e 48 is 0
-        {
-            isNumber = false;
-            break;
-        }
-    }
-
-    if (isNumber) //if TRUE
-        cout << "Number entered properly " << endl;
-    else //if FALSE
-        cout << "Number wasn't entered properly" << endl;
-
+    return 0; //we can use main() w/o return 0;
 }
 
 void welcome() //void dont return anything
 {
     cout << "Hello, welcome!" << endl;
+}
+
+bool isNumber(string tmp)
+{
+    //bool isNumber = true; //indicate true or false.
+
+    if (tmp[0] == '0')
+        return false; //hey this is not Number
+    for (int i = 0; i < tmp.length(); i++) //tmp.length() - tmp is variable, length() is function and the . is connector
+    {
+        if(!(tmp[i] >= 48 && tmp[i] <= 57)) // we used NEGATION ! //checking if set of number (IN DECIMAL) is between 0-9. i.e 48 is 0
+
+            return false;
+    }
+
+    return true;
+
+}
+
+void enterName()
+{
+    string tmp; //if ask for integer, why use string? string are characters in ASCII
+
+    cout << "Enter the number: " << endl;
+    cin >> tmp;
+
+
+    if (isNumber(tmp)) //if TRUE
+        cout << "Number entered properly " << endl;
+    else //if FALSE
+        cout << "Number wasn't entered properly" << endl;
+
 }
